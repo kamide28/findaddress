aaee982619ce6004390be34d73b65203e1c7a3d0
kamide28 <ghmaho0928@gmail.com>
2023-06-08 21:35:43 +0900

    フロント部分実装

--- src/main/java/com/example/htmlpractice/AddressController.java
+++ src/main/java/com/example/htmlpractice/AddressController.java
@@ -1,18 +1,33 @@
 package com.example.htmlpractice;
 
 import com.example.htmlpractice.model.Address;
+import com.example.htmlpractice.model.AddressData;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class AddressController {
     @Autowired
     AddressService addressService;
 
+    @GetMapping("/")
+    public String index() {
+        return "index";
+    }
+
     @GetMapping("/address")
-    public Address find(@RequestParam("zipcode") String zipcode) {
-        return addressService.findZipCode(zipcode);
+    public String find(HttpSession httpSession, Model model, @RequestParam("zipcode") String zipcode) {
+        Address address = addressService.findZipCode(zipcode);
+        model.addAttribute("addressinfo", address);
+        List<AddressData> addressData = new ArrayList<>(address.getResults());
+        model.addAttribute("addressbody", addressData);
+        return "address";
     }
 }
